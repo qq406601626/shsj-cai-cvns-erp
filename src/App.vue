f57f78df19712fc3ad512c69d80c396bb3d0dd22
guoshuai <406601626@qq.com>
2023-04-28 10:47:47 +0800

    初始化项目并修改node-sass和sass-loader版本

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <div ref="box"></div>
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {}
+}
+
+</script>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>index</div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
